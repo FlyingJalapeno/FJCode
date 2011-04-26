be5a892a9381715201d08f2115c19f50d68b8837
Corey Floyd <coreyfloyd@gmail.com>
2011-04-26 13:43:53 -0400

    convenience method for faulting an object into existence (mostly for gdb)

--- FJCode/NSManagedObject+Extensions.h
+++ FJCode/NSManagedObject+Extensions.h
@@ -16,5 +16,10 @@
  @abstract   Returns YES if this managed object is new and has not yet been saved yet into the persistent store.
  */
 -(BOOL)isNew;
+
+//faults an object into existence
+- (void)faultIntoContext;
+
+
 @end
 
