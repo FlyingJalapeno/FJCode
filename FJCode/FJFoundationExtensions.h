21caf4d41bc3dcc3fd8963062acc0b6254caec9a
Corey Floyd <coreyfloyd@gmail.com>
2012-03-29 01:27:56 -0400

    its kind of a big deal

--- FJCode/FJFoundationExtensions.h
+++ FJCode/FJFoundationExtensions.h
@@ -3,12 +3,9 @@
 #import <Foundation/Foundation.h>
 
 
-#import "NSObject+Proxy.h"
 #import "NSObjectHelper.h"
-#import "NSObject_KVOBlockNotificationExtensions.h"
 #import "NSString+extensions.h"
 #import "NSDictionaryHelper.h"
-#import "NSArray+extensions.h"
 #import "NSSet+blocks.h"
 #import "NSURL+Extensions.h"
 #import "NSOperationQueue+Shared.h"
@@ -17,6 +14,8 @@
 #import "NSTimer+Extensions.h"
 #import "NSPredicate+extensions.h"
 #import "NSIndexSet+extensions.h"
-#import"NSFetchRequest+extensions.h"
+#import "NSFetchRequest+extensions.h"
+#import "NSArray+extensions.h"
+
 //#import "NSData+Base64.h"
 
