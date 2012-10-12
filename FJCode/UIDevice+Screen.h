78f941b37693e630a6de85a410dfd790722b2fa2
Corey Floyd <coreyfloyd@gmail.com>
2012-10-11 21:32:31 -0400

    Added category to detect screens

---
+++ FJCode/UIDevice+Screen.h
@@ -0,0 +1,24 @@
+//
+//  UIDevice+Screen.h
+//  FJCode
+//
+//  Created by Corey Floyd on 10/11/12.
+//  Copyright (c) 2012 Flying Jalapeño. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+enum {
+    UIDeviceResolution_Unknown          = 0,
+    UIDeviceResolution_iPhoneStandard   = 1,    // iPhone 1,3,3GS Standard Display  (320x480px)
+    UIDeviceResolution_iPhoneRetina35   = 2,    // iPhone 4,4S Retina Display 3.5"  (640x960px)
+    UIDeviceResolution_iPhoneRetina4    = 3,    // iPhone 5 Retina Display 4"       (640x1136px)
+    UIDeviceResolution_iPadStandard     = 4,    // iPad 1,2 Standard Display        (1024x768px)
+    UIDeviceResolution_iPadRetina       = 5     // iPad 3 Retina Display            (2048x1536px)
+}; typedef NSUInteger UIDeviceResolution;
+
+@interface UIDevice (Resolutions)
+
+- (UIDeviceResolution)resolution;
+
+@end
